4bb00242552efef404b6cc31689b339411a34cad
Newtt <m.chatelan@gmail.com>
2017-11-09 15:06:25 +0100

    CLEAN START -- CLEAN START -- CLEAN START

---
+++ swagger/spring-server/src/main/java/io/pestakit/status/repositories/FruitRepository.java
@@ -0,0 +1,11 @@
+package io.pestakit.status.repositories;
+
+import io.pestakit.status.entities.FruitEntity;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Olivier Liechti on 26/07/17.
+ */
+public interface FruitRepository extends CrudRepository<FruitEntity, Long>{
+
+}
