06e70a806ae23ecd32e0d7f0d98b3515a37ffa46
Naewy <katomey@gmail.com>
2017-11-22 16:25:37 +0100

    Test finis foncitonnels avec la db

--- docker/mysql/data/createDB.sql
+++ docker/mysql/data/createDB.sql
@@ -7,7 +7,7 @@ USE status;
 CREATE TABLE service_entity (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(64) NOT NULL,
-	state VARCHAR(64) NOT NULL,
+	state VARCHAR(64),
 	description VARCHAR(128),
 	contact VARCHAR(64),
 	status_address VARCHAR(256)
