0498e0e58e591dc3375c2434227558f4035100dd
Naewy <katomey@gmail.com>
2017-11-22 15:42:14 +0100

    Test for iterations 1 finished

--- swagger/services-specs/src/test/java/io/pestakit/status/api/spec/steps/CreationSteps.java
+++ swagger/services-specs/src/test/java/io/pestakit/status/api/spec/steps/CreationSteps.java
@@ -5,62 +5,120 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.pestakit.status.ApiException;
 import io.pestakit.status.ApiResponse;
+import io.pestakit.status.api.ServicesApi;
+import io.pestakit.status.api.dto.ServiceGet;
+import io.pestakit.status.api.dto.ServicePost;
 import io.pestakit.status.api.spec.helpers.Environment;
+
+import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by Olivier Liechti on 27/07/17.
- */
-public class CreationSteps {
+public class CreationSteps
+{
+   private Environment environment;
+   private ServicesApi api;
 
-    /*
-    private Environment environment;
-    private DefaultApi api;
+   private ServicePost service;
+   private List<ServiceGet> services;
+   private int size;
 
-    Fruit fruit;
+   private ApiResponse lastApiResponse;
+   private ApiException lastApiException;
+   private boolean lastApiCallThrewException;
+   private int lastStatusCode;
 
-    private ApiResponse lastApiResponse;
-    private ApiException lastApiException;
-    private boolean lastApiCallThrewException;
-    private int lastStatusCode;
+   public CreationSteps(Environment environment)
+   {
+      this.environment = environment;
+      this.api = environment.getApi();
+   }
 
-    public CreationSteps(Environment environment) {
-        this.environment = environment;
-        this.api = environment.getApi();
-    }
+   @Given("^there is a Services server$")
+   public void there_is_a_Services_server() throws Throwable
+   {
+      assertNotNull(api);
+   }
 
-    @Given("^there is a Fruits server$")
-    public void there_is_a_Fruits_server() throws Throwable {
-        assertNotNull(api);
-    }
+   @Given("^I have a service payload$")
+   public void i_have_a_service_payload() throws Throwable
+   {
+      service = new ServicePost();
+      service.setName("Test");
+      service.setStatusAddress("10.10.10.3");
+   }
 
-    @Given("^I have a fruit payload$")
-    public void i_have_a_fruit_payload() throws Throwable {
-        fruit = new io.avalia.fruits.api.dto.Fruit();
-    }
+   @Given("^I have an invalid service payload$")
+   public void i_have_an_invalid_service_payload() throws Throwable
+   {
+      service = new ServicePost();
+      service.setName("Invalid test");
+      service.setStatusAddress(null);
+   }
 
-    @When("^I POST it to the /fruits endpoint$")
-    public void i_POST_it_to_the_fruits_endpoint() throws Throwable {
-        try {
-            lastApiResponse = api.createFruitWithHttpInfo(fruit);
-            lastApiCallThrewException = false;
-            lastApiException = null;
-            lastStatusCode = lastApiResponse.getStatusCode();
-        } catch (ApiException e) {
-            lastApiCallThrewException = true;
-            lastApiResponse = null;
-            lastApiException = e;
-            lastStatusCode = lastApiException.getCode();
-        }
+   @Given("^I recuperate the actual size of the service list$")
+   public void i_recuperate_the_actual_size_of_the_service_list() throws Throwable {
+      i_GET_on_services_endpoint();
+      size = services.size();
+   }
 
-    }
+   @When("^I POST it to the /services endpoint$")
+   public void i_POST_it_to_the_services_endpoint() throws Throwable
+   {
+      try
+      {
+         lastApiResponse = api.addServiceWithHttpInfo(service);
+         lastApiCallThrewException = false;
+         lastApiException = null;
+         lastStatusCode = lastApiResponse.getStatusCode();
+      }
+      catch (ApiException e)
+      {
+         lastApiCallThrewException = true;
+         lastApiResponse = null;
+         lastApiException = e;
+         lastStatusCode = lastApiException.getCode();
+      }
+   }
 
-    @Then("^I receive a (\\d+) status code$")
-    public void i_receive_a_status_code(int arg1) throws Throwable {
-        assertEquals(201, lastStatusCode);
-    }
-    */
+   @When("^I GET on /services endpoint$")
+   public void i_GET_on_services_endpoint() throws Throwable
+   {
+      try
+      {
+         lastApiResponse = api.getServicesWithHttpInfo(null);
+         lastApiCallThrewException = false;
+         lastApiException = null;
+         lastStatusCode = lastApiResponse.getStatusCode();
+         services = (List<ServiceGet>) lastApiResponse.getData();
+      }
+      catch (ApiException e)
+      {
+         lastApiCallThrewException = true;
+         lastApiResponse = null;
+         lastApiException = e;
+         lastStatusCode = lastApiException.getCode();
+      }
+   }
 
+   @Then("^I receive a (\\d+) status code$")
+   public void i_receive_a_status_code(int arg1) throws Throwable
+   {
+      assertEquals(arg1, lastStatusCode);
+   }
+
+   @Then("^I receive a list of services$")
+   public void i_receive_a_list_of_services() throws Throwable
+   {
+      assertNotNull(services);
+   }
+
+   @Then("^The size should be greater by one$")
+   public void the_size_should_be_greater_by_one() throws Throwable {
+      i_GET_on_services_endpoint();
+      int new_size = services.size();
+
+      assertEquals(size + 1, new_size);
+   }
 }
