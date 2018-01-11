771cb51543ccc5c0bd5c462a84eea83a334f1c18
Newtt <m.chatelan@gmail.com>
2018-01-11 15:13:55 +0100

    task scheduler

--- swagger/spring-server/src/main/java/io/pestakit/status/agent/ScheduledTasks.java
+++ swagger/spring-server/src/main/java/io/pestakit/status/agent/ScheduledTasks.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.net.Socket;
 import java.util.List;
 
@@ -29,47 +30,66 @@ public class ScheduledTasks
    {
       System.out.println("Starting service check...");
 
+      // Retrieve all services
       services = serviceRepository.findAll();
 
+      // Check all services
       for(ServiceEntity service : services)
       {
          // If the service is required to be checked
          if(isCheckable(service))
          {
+            // Check if the service is alive
             boolean alive = isAlive(service);
 
-            ServiceEntity updatedService = new ServiceEntity(service);
-
-            // Set the appropriate state
-            if(alive)
-               updatedService.setState(State.UP);
-            else
-               updatedService.setState(State.DOWN);
-
-            // Delete the old service
-            serviceRepository.deleteByUid(service.getUid());
-
-            // Restore the same uid for the updated service
-            updatedService.setUid(service.getUid());
-
-            // Save the updated service
-            serviceRepository.save(updatedService);
+            // Update the service
+            updateService(service, alive);
          }
       }
 
+      System.out.println("End of the services check");
    }
 
    private boolean isCheckable(ServiceEntity serviceEntity)
    {
-      boolean addressState = !serviceEntity.getStatusAddress().equals("") && serviceEntity.getStatusAddress() != null;
-      return true;
+      boolean address = !serviceEntity.getStatusAddress().equals("") && serviceEntity.getStatusAddress() != null;
+      boolean port = serviceEntity.getStatusPort() != null;
+
+      return port && address;
    }
 
-   private boolean isAlive(ServiceEntity serviceEntity)
+   private boolean isAlive(ServiceEntity service)
    {
-      System.out.println("Checking service " + serviceEntity.getName());
+      System.out.println("Checking service " + service.getName());
 
-      Socket socket =
-      return true;
+      try (Socket s = new Socket(service.getStatusAddress(), service.getStatusPort())) {
+         return true;
+      } catch (IOException ex) {
+
+      }
+      return false;
+   }
+
+   private void updateService(ServiceEntity oldService, boolean alive) {
+
+      System.out.println("Updating service " + oldService.getName());
+
+      // Create an updated service
+      ServiceEntity updatedService = new ServiceEntity(oldService);
+
+      // Set all the appropriate fields and update the required ones
+      updatedService.setUid(oldService.getUid());
+
+      // Set the appropriate state
+      if(alive)
+         updatedService.setState(State.UP);
+      else
+         updatedService.setState(State.DOWN);
+
+      // Delete the old service
+      serviceRepository.deleteByUid(oldService.getUid());
+
+      // Save the updated service
+      serviceRepository.save(updatedService);
    }
 }
